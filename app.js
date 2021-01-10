365f893c1e5f68fe260029a79d361feb38ba3841
Russell <russelldev@protonmail.com>
2021-01-10 07:11:40 +0000

    Add ability to fill up main cups meter

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const smallCups = document.querySelectorAll('.cup-small');
-const l = document.getElementById('#l');
-const percentage = document.getElementById('#percentage');
-const remained = document.getElementById('#remained');
+const l = document.getElementById('l');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+updateBigCup();
 
 smallCups.forEach(function(cup, i) {
     cup.addEventListener('click', () => highlightCups(i));
@@ -19,4 +21,28 @@ function highlightCups(i) {
             cup.classList.remove('full');
         }
     });
+    updateBigCup();
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    const totalCups = smallCups.length;
+
+    if (fullCups === 0) {
+        percentage.style.visibility = 'hidden';
+        percentage.style.height = 0;
+    } else {
+        percentage.style.visibility = 'visible';
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden';
+        remained.style.height = 0;
+    } else {
+        remained.style.visibility = 'visible';
+        remained.style.height = 1;
+        l.innerText = `${250 * fullCups / 1000}L`
+    }
 }
