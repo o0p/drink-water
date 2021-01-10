82a71414b0654ce69e460026aa0521424ac18c78
Russell <russelldev@protonmail.com>
2021-01-10 06:20:54 +0000

    Add var selectors

---
+++ app.js
@@ -0,0 +1,4 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const l = document.getElementById('#l');
+const percentage = document.getElementById('#percentage');
+const remained = document.getElementById('#remained');
