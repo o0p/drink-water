21e97ab7c44d62e090c4c03f5d23ce9c7d8d45f7
Russell <russelldev@protonmail.com>
2021-01-10 06:41:50 +0000

    Add ability to highlight drank cups

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const l = document.getElementById('#l');
 const percentage = document.getElementById('#percentage');
 const remained = document.getElementById('#remained');
 
-smallCups.forEach((cup, i) => {
-    console.log(i)
+smallCups.forEach(function(cup, i) {
+    cup.addEventListener('click', () => highlightCups(i));
 });
+
+function highlightCups(i) {
+
+    smallCups.forEach(function(cup, i2){
+        if (i2 <= i) {
+            cup.classList.add('full');
+        } else {
+            cup.classList.remove('full');
+        }
+    });
+}
